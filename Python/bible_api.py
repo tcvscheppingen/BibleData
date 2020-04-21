cd229f73b242407df2060042de72e35ab2ff1dd2
tcvscheppingen <tcvscheppingen@student.che.nl>
2020-04-21 08:08:09 +0200

    Added functions: get_book(), get_chapter() and search_group()

--- Python/bible_api.py
+++ Python/bible_api.py
@@ -26,9 +26,34 @@ def book_listing(key, dam_id):
     return response
 
 
+def get_book(key, dam_id, book_id):
+    # Retrieves a JSON formated Bible book.
+    response = requests.get('https://dbt.io/text/verse?key=' + key + '&dam_id=' +
+                            dam_id + '&book_id=' + book_id + '&v=2').json()
+
+    return response
+
+
+def get_chapter(key, dam_id, book_id, chapter_id):
+    # Retrieves a JSON formatted selection of a chapter from a Bible book.
+    response = requests.get('https://dbt.io/text/verse?key=' + key + '&dam_id=' +
+                            dam_id + '&book_id=' + book_id + '&chapter_id=' + chapter_id + '&v=2').json()
+
+    return response
+
+
 def get_verse(key, dam_id, book_id, chapter_id, verse_start, verse_end):
     # Retrieves a JSON formatted selection of Bible verses.
     response = requests.get(
         'https://dbt.io/text/verse?key=' + key + '&dam_id=' + dam_id + '&book_id=' + book_id + '&chapter_id=' + chapter_id + '&verse_start=' + verse_start + '&verse_end=' + verse_end + '&v=2').json()
+
+    return response
+
+
+def search_group(key, dam_id, query):
+    # Searches the Bible based on a given query.
+    # Returns total results, results per book and the first result in every book.
+    response = requests.get('https://dbt.io/text/searchgroup?key=' +
+                            key + '&dam_id=' + dam_id + '&query=' + query + '&v=2').json()
 
     return response
